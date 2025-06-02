7039b2c530942903506aa316e8123df73720137d
Farhan Afzal <faru.3278@gmail.com>
2025-06-02 21:52:49 +0500

    Authentication Included

---
+++ resources/views/layouts/sidebar.blade.php
@@ -0,0 +1,19 @@
+<!-- resources/views/layouts/sidebar.blade.php -->
+<aside class="position-fixed top-0 start-0" style="top: 64px; width: 220px; height: calc(100vh - 64px); z-index: 1030;">
+    <nav class="bg-light rounded-end shadow-sm h-100 d-flex flex-column p-3">
+        <ul class="nav flex-column">
+            <li class="nav-item mb-3">
+                <a class="nav-link fw-medium text-dark" href="#">Dashboard</a>
+            </li>
+            <li class="nav-item mb-3">
+                <a class="nav-link fw-medium text-dark" href="#">Tasks</a>
+            </li>
+            <li class="nav-item mb-3">
+                <a class="nav-link fw-medium text-dark" href="#">Projects</a>
+            </li>
+            <li class="nav-item mb-3">
+                <a class="nav-link fw-medium text-dark" href="#">Settings</a>
+            </li>
+        </ul>
+    </nav>
+</aside>
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\TaskController;
-use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,18 +14,27 @@ use Illuminate\Http\Request;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
-Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
-Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
-Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
-Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
-Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
-Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
-// Route::get('/tasks/filter', [TaskController::class, 'filter'])->name('tasks.filter');
-Route::get('/tasks/filter', function (Request $request) {
-    dd(1);
-})->name('tasks.filter');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    //Tasks
+    Route::get('/tasks', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+    Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
+    Route::get('/tasks/{id}', [TaskController::class, 'show'])->name('tasks.show');
+    Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
+    Route::put('/tasks/{id}', [TaskController::class, 'update'])->name('tasks.update');
+    Route::delete('/tasks/{id}', [TaskController::class, 'destroy'])->name('tasks.destroy');
+});
+
+require __DIR__.'/auth.php';
